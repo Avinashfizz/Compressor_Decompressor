cba6fcf1f52711723aecc3a36898735e20355a1b
Avinashfizz <130842159+Avinashfizz@users.noreply.github.com>
2023-04-15 18:15:30 +0530

    Add files via upload

---
+++ compressor and decompressor/src/comp_decomp/compressor.java
@@ -0,0 +1,37 @@
+package comp_decomp;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.zip.GZIPOutputStream;
+
+
+
+public class compressor {
+
+    public static void method(File file) throws IOException{
+        String fileDirectory=file.getParent();
+
+        FileInputStream fis=new FileInputStream(file);
+        FileOutputStream fos=new FileOutputStream(fileDirectory+"/compressedFile.gz");
+        GZIPOutputStream gzip=new GZIPOutputStream(fos);
+
+        byte [] buffer=new byte[1024];
+        int len;
+
+        while((len=fis.read(buffer))!=-1){
+            gzip.write(buffer, 0, len);
+        }
+        gzip.close();
+        fos.close();
+        fis.close();
+
+
+    }
+
+    public static void main(String[] args){
+        File path = new File("/Users/Shanaya/Downloads/Compressor/compressordecompressor/Testfile");
+
+    }
+}
